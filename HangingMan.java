bc70436ed9510717074ae6d8839fa55898d3c3f5
mewert03@gmail.com <mewert03@gmail.com>
2021-03-13 16:07:31 -0800

    Initial sharing of project

---
+++ HangingMan.java
@@ -0,0 +1,62 @@
+public class HangingMan {
+
+    private int numWrongGuesses;
+    String[] HangmanImage = {"+----+\n"+
+            "|\n"+
+            "|\n"+
+            "|\n"+
+            "/\\\n",
+
+            "+----+\n"+
+            "|    O\n"+
+            "|\n"+
+            "|\n"+
+            "/\\\n",
+
+            "+----+\n"+
+            "|    O\n"+
+            "|    +\n"+
+            "|\n"+
+            "/\\\n",
+
+            "+----+\n"+
+            "|    O\n"+
+            "|    +-\n"+
+            "|\n"+
+            "/\\\n",
+
+            "+----+\n"+
+            "|    O\n"+
+            "|   -+-\n"+
+            "|\n"+
+            "/\\\n",
+
+            "+----+\n"+
+            "|    O\n"+
+            "|   -+-\n"+
+            "|   / \n"+
+            "/\\\n",
+
+            "+----+\n"+
+            "|    O\n"+
+            "|   -+-\n"+
+            "|   / \\\n"+
+            "/\\\n",
+        };
+
+    HangingMan(){
+
+    }
+    
+    public void show(){
+        
+    }
+
+    public boolean isntDead(){
+        return true;
+    }
+    
+    public void dieSomeMore(){
+        
+    }
+}
---
+++ Puzzle.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Puzzle{
+
+    Puzzle(){
+
+    }
+
+    public void show(){
+
+    }
+
+    public String getWord(){
+        return "";
+    }
+
+    public boolean isUnsolved(){
+        return true;
+    }
+
+    public boolean makeGuess(String g){
+        return true;
+    }
+
+    
+}
